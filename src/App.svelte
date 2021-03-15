93a24962767afe9b8e996cd4f81a273cc32d6167
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-03-15 15:13:42 -0600

    Fixed case sensitive import

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 	import Movies from './Components/Movies.svelte';
 	import Modal from 'svelte-simple-modal';
 	import { Router, Link, Route } from "svelte-routing";
-	import MyWatchList from './Components/MyWatchList.svelte.ts';
+	import MyWatchList from './Components/MyWatchlist.svelte';
 	export let url = "";
 </script>
 
