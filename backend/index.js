33658b6a261681f8ca755e8c06433d6c07a7332d
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-03-15 15:32:32 -0600

    Serving up public directory

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const mongoose = require("mongoose")
 const movieRoutes = require("./routes")
 const bodyParser = require("body-parser")
-const PORT = 3000;
 const dotenv = require('dotenv')
 const cors = require('cors')
 mongoose.connect('mongodb+srv://dbAdmin:p5UoCrjcL8Fw394z@watchlydb.cyhzd.mongodb.net/watchly?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true  })
@@ -12,6 +11,7 @@ mongoose.connect('mongodb+srv://dbAdmin:p5UoCrjcL8Fw394z@watchlydb.cyhzd.mongodb
     app.use(cors())
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static('public'))
     app.use("/api", movieRoutes)
     app.listen(process.env.PORT || 3000, () => {
         console.log("Server has started")
