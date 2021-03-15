da78b24e5dad1c0eea4460872591f4ccae509b32
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-03-15 16:06:34 -0600

    Served index.html file.

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,13 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 mongoose.connect('mongodb+srv://dbAdmin:p5UoCrjcL8Fw394z@watchlydb.cyhzd.mongodb.net/watchly?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true  })
 .then(() => {
-    app.set('port', (process.env.PORT || 3000));
     dotenv.config()
     const app = express()
+    app.set('port', (process.env.PORT || 3000));
     app.use(cors())
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(express.static("public"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "index.html"));
-    });
     app.use("/api", movieRoutes)
     app.listen(app.get('port'), () => {
         console.log("Server has started")
