20d5de6419296e25900982860378dd25f84fa1c4
Tyler Ivie <59585410+tivie001@users.noreply.github.com>
2021-03-15 15:51:58 -0600

    Served index.html file.

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ mongoose.connect('mongodb+srv://dbAdmin:p5UoCrjcL8Fw394z@watchlydb.cyhzd.mongodb
     app.use(cors())
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
-    app.use('/build', express.static('public/build'))
+    app.use(express.static("public"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    });
     app.use("/api", movieRoutes)
     app.listen(process.env.PORT || 3000, () => {
         console.log("Server has started")
